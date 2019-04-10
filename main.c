69cb31c4ae92a736bbfac19ebeefff2587144e39
Jack Foltz <jack@foltz.io>
2019-04-09 23:10:02 -0400

    Remove idt stuff from main

--- main.c
+++ main.c
@@ -2,18 +2,14 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-#include "idt.h"
-
 MODULE_LICENSE("GPL"); // nt torvalds
 
 static int __init overlord_init(void) {
-    idt_setup();
     printk(KERN_INFO "[Overlord]: Hello World!\n");
     return 0;
 }
 
 static void __exit overlord_exit(void) {
-    idt_rollback();
     printk(KERN_INFO "[Overlord]: Goodbye World!\n");
 }
 
