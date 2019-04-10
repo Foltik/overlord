c410ce2efa6f6abdf4ba78bca840725ec5e18ea6
Jack Foltz <jack@foltz.io>
2019-04-10 00:48:31 -0400

    Add syscall to makefile

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#include "syscall.h"
+
 MODULE_LICENSE("GPL"); // nt torvalds
 
 static int __init overlord_init(void) {
+    syscall_setup();
     printk(KERN_INFO "[Overlord]: Hello World!\n");
     return 0;
 }
 
 static void __exit overlord_exit(void) {
+    syscall_rollback();
     printk(KERN_INFO "[Overlord]: Goodbye World!\n");
 }
 
