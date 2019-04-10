62505fd167fd78ad094df89afbc2231cf1aeb340
Jack Foltz <jack@foltz.io>
2019-04-09 22:40:35 -0400

    Remove build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MAP=/lib/modules/$(uname -r)/build/System.map
-SYSCALL_TABLE=0x$(grep "R sys_call_table" $MAP | awk '{print $1}')
-
-> offsets.h
-echo "#define OFFSET_SYSCALL_TABLE $SYSCALL_TABLE" >> offsets.h
-
-make
